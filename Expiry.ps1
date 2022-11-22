293104a234984646de66d896f12078d156d61b4c
J/ <55988027+jkerai1@users.noreply.github.com>
2022-11-22 16:32:51 +0000

    Update Expiry.ps1

--- Expiry.ps1
+++ Expiry.ps1
@@ -2,7 +2,7 @@
 
 $url = "https://www.microsoft.com/" 
 $req = [Net.HttpWebRequest]::Create($url)
-
+$req.GetResponse()
 ##$req.ServicePoint.Certificate | gm | where{$_.Name -like "*Date*"}
 
 $req.ServicePoint.Certificate.GetExpirationDateString()
