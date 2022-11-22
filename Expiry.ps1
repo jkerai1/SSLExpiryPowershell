6343f5f4c92777db9696e30c19124526082a8f45
J/ <55988027+jkerai1@users.noreply.github.com>
2022-11-22 16:38:00 +0000

    Update Expiry.ps1

--- Expiry.ps1
+++ Expiry.ps1
@@ -1,10 +1,8 @@
 [Net.ServicePointManager]::ServerCertificateValidationCallback = { $true }
-$url = "https://www.microsoft.com/"
+$url = "https://www.microsoft.com/" #Example
 $req = [Net.HttpWebRequest]::Create($url)
 $req.GetResponse() | Out-Null
 $output = [PSCustomObject]@{
-   URL = $url
-   'Cert Start Date' = $req.ServicePoint.Certificate.GetEffectiveDateString()
-   'Cert End Date' = $req.ServicePoint.Certificate.GetExpirationDateString()
-}
+   #'Cert Start Date' = $req.ServicePoint.Certificate.GetEffectiveDateString()
+   'Cert End Date' = $req.ServicePoint.Certificate.GetExpirationDateString()}
 $output
